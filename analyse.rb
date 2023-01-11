503a334001938ecbfee7558db60391ab5f80b088
Gaétan Basile <gaetan.js.basile@outlook.fr>
2023-01-11 17:53:01 +0100

    ✨ Add accepting parameters

--- analyse.rb
+++ analyse.rb
@@ -4,8 +4,26 @@ require "rubygems"
 require "bundler/setup"
 
 require "fileutils"
-
 require "byebug"
+
+SCRIPT_VERSION = "0.1.0"
+SCRIPT_URL = "https://github.com/Kisoke/atelier-item-library-tools"
+
+# options parser
+require "./utils/options_parser"
+
+# config params
+MODES =
+  if @command_options[:mix]
+    %w[mix]
+  elsif @command_options[:mat]
+    %w[mat]
+  else
+    %w[mat mix]
+  end.freeze
+
+INPUT_FOLDER = @command_options[:input].freeze
+OUTPUT_FOLDER = @command_options[:output].freeze
 
 # Inputs
 require "./inputs/library_images_folder"
@@ -16,20 +34,20 @@ require "./outputs/dictionnary_json_file"
 # processors
 require "./processors/library_images_tesseract"
 
-SCRIPT_VERSION = "0.1.0"
-SCRIPT_URL = "https://github.com/Kisoke/atelier-item-library-tools"
-
 OUTPUT_PROCESSORS = [
   Outputs::ArrayJSONFile,
   Outputs::DictionnaryJSONFile
 ].freeze
 
-def library_extract
+def library_extract(folder)
   # remove old tempfiles
   FileUtils.rm_rf(Inputs::LibraryImagesFolder::TEMP_IMAGES_DIR)
+
+  raise "Input directory does not exist" unless Dir.exists?(INPUT_FOLDER)
+
   Dir.mkdir(Inputs::LibraryImagesFolder::TEMP_IMAGES_DIR)
 
-  input = Inputs::LibraryImagesFolder.new("mix")
+  input = Inputs::LibraryImagesFolder.new(folder)
 
   processed = Processors::LibraryImagesTesseract.process(input.entries)
 
@@ -41,4 +59,4 @@ def library_extract
   end
 end
 
-library_extract
+MODES.each { |mode| library_extract mode }
